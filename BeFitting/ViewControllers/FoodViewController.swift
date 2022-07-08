724b4a784d713f9c04426cc7b4a98cf5fb4f90ef
dukidule <87377702+dukidule@users.noreply.github.com>
2022-07-08 00:32:33 -0700

    Fixed button, made popup
    Fixed button that was refusing to display its title, made a popup for when food name, quantity and calories values are not inserted in the text fields.

--- BeFitting/ViewControllers/FoodViewController.swift
+++ BeFitting/ViewControllers/FoodViewController.swift
@@ -17,6 +17,9 @@ class FoodViewController: UIViewController {
     //ContentView
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var measurementSelectionView: UIView!
+    //PopupView
+    @IBOutlet weak var popUpView: UIView!
+    
     //Labels
     
     
@@ -41,6 +44,7 @@ class FoodViewController: UIViewController {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var removeButton: UIButton!
     
+    @IBOutlet weak var okPopupButton: UIButton!
     // UITableView
     @IBOutlet weak var measurementsTableView: UITableView!
     
@@ -50,6 +54,7 @@ class FoodViewController: UIViewController {
         if food.measurement == "" {
             food.measurement = "g"
         }
+        if missingParameters() ==  true {
         
         food.name = nameTextField.text ?? ""
         food.quantity = quantityTextField.text ?? ""
@@ -59,9 +64,21 @@ class FoodViewController: UIViewController {
         food.fats = fatsTextField.text ?? ""
         passingFoodDelegate.passFood(food: food, id: tableId)
         self.dismiss(animated: true, completion: nil)
+        } else {
+            contentView.alpha = 0.5
+            contentView.isUserInteractionEnabled = false
+            popUpView.isHidden = false
+        }
         print(food)
         print("eat dicc" + tableId)
     }
+    @IBAction func okPopUpButtonTapped(_ sender: UIButton) {
+        popUpView.isHidden = true
+        contentView.isUserInteractionEnabled = true
+        contentView.alpha = 1
+    }
+    
+    
     @IBAction func cancelButtonClicked(_ sender: UIButton) {
         
         
@@ -78,7 +95,7 @@ class FoodViewController: UIViewController {
     @IBAction func showMeasurementChoices(_ sender: UIButton) {
         measurementSelectionView.isHidden = false
     }
-    var measurements: [String] = [("g"), ("oz")]
+    var measurements: [String] = [("g"), ("Oz")]
     var passingFoodDelegate: PassingFood!
     var tableId = ""
     var food = FoodLog(name: "", calories: "", measurement: "", quantity: "", protein: "", carbs: "", fats: "")
@@ -96,7 +113,8 @@ class FoodViewController: UIViewController {
         carbsTextField.text = food.carbs
         fatsTextField.text = food.fats
         measurementSelectionView.isHidden = true
-        
+        quantitySelectionButton.setTitle("g", for: .normal)
+                
         print(tableId)
         
     }
@@ -119,7 +137,95 @@ class FoodViewController: UIViewController {
 //            }
 //            }
 //        }
+    
+    func nameMissing() -> Bool {
+        if nameTextField.text == "" {
+            return true
+        } else {
+            return false
+        }
     }
+    
+    func quantityMissing() -> Bool {
+        if quantityTextField.text == "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func caloriesMisisng() -> Bool {
+        if caloriesTextField.text == "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func missingParameters() -> Bool {
+        var a = 0
+        if nameMissing() == true {
+            a = a+1
+        }
+        if quantityMissing() == true {
+            a = a+2
+        }
+        if caloriesMisisng() == true {
+            a=a+5
+        }
+        switch a {
+        case 0:
+            return true
+        case 1:
+            print("Food name field is necessary.")
+            return false
+        case 2:
+            print("Food quantity field is necessary.")
+            return false
+        case 3:
+            print("Food name and quantity fields are necessary.")
+            return false
+        case 5:
+            print("Calories field is necessary.")
+            return false
+        case 6:
+            print("Food name and calories fields are necessary.")
+            return false
+        case 7:
+            print("Food quantity and calories fields are necessary")
+            return false
+        case 8:
+            print("Food name, quantity and calories fields are necessary.")
+            return false
+        default:
+            print("You f'd up mate..")
+            return false
+        }
+        
+    }
+    
+//    func missingParameters() -> Bool {
+//        var a = 0
+//        if nameTextField.text == "" {
+//            print("addFoodPLs")
+//            return false
+//            //1
+//        }
+//        else if quantityTextField.text == "" {
+//            print("addQuantityPls")
+//            return false
+//            //2
+//        }
+//        else if caloriesTextField.text == "" {
+//            print ("addCalsPLs")
+//            return false
+//            //3
+//        } else {
+//            return true
+//        }
+//
+//    }
+}
   
 
 //MARK: - Table View Code
@@ -143,6 +249,9 @@ extension FoodViewController: UITableViewDelegate {
         let measure = measurements[indexPath.row]
         food.measurement = measure
         measurementSelectionView.isHidden = true
+        if quantitySelectionButton.titleLabel?.text != measure {
+            quantitySelectionButton.setTitle(measure, for: .normal)
+        }
         
     }
 }
